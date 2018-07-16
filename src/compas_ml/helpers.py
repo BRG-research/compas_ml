610537970dec42e1d8ae078981efa6c51ebb456f
andrewtliew <liew@arch.ethz.ch>
2018-07-16 15:07:32 +0200

    - colour_weights() function helper.

--- src/compas_ml/helpers.py
+++ src/compas_ml/helpers.py
@@ -3,6 +3,14 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from matplotlib import pyplot as plt
+
+from numpy import array
+from numpy import float32
+from numpy import zeros
+
+from scipy.misc import imread
+
 
 __author__    = ['Andrew Liew <liew@arch.ethz.ch>']
 __copyright__ = 'Copyright 2018, BLOCK Research Group - ETH Zurich'
@@ -12,6 +20,7 @@ __email__     = 'liew@arch.ethz.ch'
 
 __all__ = [
     'classes_to_onehot',
+    'colour_weights',
 ]
 
 
@@ -41,10 +50,43 @@ def classes_to_onehot(classes, length):
     return classes
 
 
+def colour_weights(path):
+
+    """ Colour a weights image to show positive (blue) and negative (red) areas.
+
+    Parameters
+    ----------
+    path : str
+        Path of weights image to load.
+
+    Returns
+    -------
+    None
+
+    """
+
+    image = array(imread(path), dtype=float32) / 255
+    dimx, dimy, dimz = image.shape
+
+    image_col = zeros([dimx, dimy, dimz])
+    image_col[:, :, 3] = 1
+    for i in range(dimx):
+        for j in range(dimy):
+            if image[i, j, 1] < 0.5:
+                val = (0.5 - image[i, j, 1]) * 2
+                image_col[i, j, 0] = val
+            elif image[i, j, 1] > 0.5:
+                val = (image[i, j, 1] - 0.5) * 2
+                image_col[i, j, 2] = val
+
+    plt.imshow(image_col)
+    plt.show()
+
+
 # ==============================================================================
 # Main
 # ==============================================================================
 
 if __name__ == "__main__":
 
-    pass
+    colour_weights(path='/home/al/downloads/ss.png')
