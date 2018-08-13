2bfdeb38c684db187986648670a548152a6e7a24
andrewtliew <liew@arch.ethz.ch>
2018-08-13 22:39:16 +0200

    - Placeholder.

---
+++ src/compas_ml/generators/gan.py
@@ -0,0 +1,24 @@
+
+
+__author__    = ['Andrew Liew <liew@arch.ethz.ch>']
+__copyright__ = 'Copyright 2018, BLOCK Research Group - ETH Zurich'
+__license__   = 'MIT License'
+__email__     = 'liew@arch.ethz.ch'
+
+
+__all__ = [
+    'gan',
+]
+
+
+def gan():
+    pass
+    
+    
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == '__main__':
+
+    pass
